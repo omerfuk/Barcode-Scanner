a06977db4af979d2c6885d8bb7ebb2fe4fae6efd
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-20 22:01:09 +0300

    Initial Commit

---
+++ Barcode Scanner/Barcode_ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barcode_ScannerApp.swift
+//  Barcode Scanner
+//
+//  Created by Ömer Faruk Kılıçaslan on 20.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct Barcode_ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
